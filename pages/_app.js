4648ee99b3747c76a2769856dfe22f4fe838f762
ayilmz <ayilmaz@ebay.com>
2022-06-25 13:46:58 +0300

    remove duplicate prop

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700;900&display=swap" rel="stylesheet" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700;900&display=swap" rel="stylesheet" />
         </Head>
         <Component {...pageProps} />
       </>
