ad89fe91e4fe7d4dcf80416a5cb720a3e5335c88
ayilmz <ayilmaz@ebay.com>
2022-06-27 11:57:44 +0300

    form elements

--- organisms/Form/index.js
+++ organisms/Form/index.js
@@ -1,6 +1,10 @@
-import {StyledFormContainer, StyledProgress} from "./style"
+import { useForm } from "react-hook-form";
+import {StyledForm, StyledFormContainer, StyledProgress} from "./style"
 
 const Form = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     return (
         <div className="row">
             <div className="col-12">
@@ -19,6 +23,52 @@ const Form = () => {
                             Ödeme
                         </li>
                     </StyledProgress>
+                    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+                        <ul className="form-elements">
+                            <li>
+                                <p>Anne Adı Soyadı</p>
+                                <input type="text" placeholder="" {...register("Anne Adı Soyadı", {required: true, maxLength: 80})} />
+                            </li>
+                            <li>
+                                <p>Anne Bedeni</p>
+                                <select {...register("Anne Bedeni", { required: true })}>
+                                    <option value="S">S</option>
+                                    <option value="M">M</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
+                                </select>
+                            </li>
+                            <li>
+                                <p>Terlik Numarası</p>
+                                <select {...register("Terlik Numarası", { required: true })}>
+                                    <option value="32">32</option>
+                                    <option value="34">34</option>
+                                    <option value="35">35</option>
+                                    <option value="36">36</option>
+                                </select>
+                            </li>
+                            <li>
+                                <p>Renk Seçimi</p>
+                                <select {...register("Renk Seçimi", { required: true })}>
+                                    <option value="Pudra Pembe">Pudra Pembe</option>
+                                    <option value="Mor">Mor</option>
+                                    <option value="Lila">Lila</option>
+                                    <option value="Kırmızı">Kırmızı</option>
+                                </select>
+                            </li>
+                        </ul>
+                        <ul className="form-submit">
+                            <li className="d-flex">
+                                <input {...register("Kisisel Verilerim", { required: true })} type="checkbox" value="Yes" id="kisisel" />
+                                <label for="kisisel">
+                                    Kişisel verilerimin <a href="#">kullanım koşullarında</a> belirtilen amaçlar için işlenmesine onay veriyorum.
+                                </label>
+                            </li>
+                            <li>
+                                <input type="submit" value="Devam Et"/>
+                            </li>
+                        </ul>
+                    </StyledForm>
                 </StyledFormContainer>
             </div>
         </div>
--- organisms/Form/style.js
+++ organisms/Form/style.js
@@ -6,6 +6,7 @@ export const StyledFormContainer = styled.div`
   border-radius: 24px 24px 0 0 ;
   margin-top: 32px;
   padding: 32px;
+  display: inline-block;
 `;
 
 export const StyledProgress = styled.ul`
@@ -33,3 +34,93 @@ export const StyledProgress = styled.ul`
     }
   }
 `;
+
+export const StyledForm = styled.form`
+  ul.form-elements{
+    padding: 0;
+    margin: 0;
+    li{
+      display: inline-block;
+      width: 50%;
+      margin-bottom: 15px;
+      &:nth-child(even){
+        text-align: right;
+        p{
+          padding-left: 20px;
+        }
+      }
+      p{
+        margin: 0 0 8px 0;
+        font-family: 'M PLUS Rounded 1c', sans-serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 14px;
+        line-height: 17px;
+        color: #E53732;
+        padding-left: 10px;
+        text-align: left;
+      }
+      input, select{
+        width: 95%;
+        background: #FFFFFF;
+        border: 1px solid #FEA38E;
+        border-radius: 40px;
+        padding: 10px 15px;
+        font-family: 'M PLUS Rounded 1c', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 18px;
+        letter-spacing: 0.01em;
+        color: #202F58;
+      }
+      select{
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iOCIgdmlld0JveD0iMCAwIDEyIDgiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik01Ljk4MTI3IDcuMDNDNS43MTU1OSA3LjAzIDUuNDQ5OTEgNi45MzIxMiA1LjI0MDE2IDYuNzIyMzdMMC4zMDQxMzIgMS43ODYzNEMtMC4xMDEzNzcgMS4zODA4MyAtMC4xMDEzNzcgMC43MDk2NDIgMC4zMDQxMzIgMC4zMDQxMzJDMC43MDk2NDIgLTAuMTAxMzc3IDEuMzgwODMgLTAuMTAxMzc3IDEuNzg2MzQgMC4zMDQxMzJMNS45ODEyNyA0LjQ5OTA2TDEwLjE3NjIgMC4zMDQxMzJDMTAuNTgxNyAtMC4xMDEzNzcgMTEuMjUyOSAtMC4xMDEzNzcgMTEuNjU4NCAwLjMwNDEzMkMxMi4wNjM5IDAuNzA5NjQyIDEyLjA2MzkgMS4zODA4MyAxMS42NTg0IDEuNzg2MzRMNi43MjIzNyA2LjcyMjM3QzYuNTEyNjIgNi45MzIxMiA2LjI0Njk1IDcuMDMgNS45ODEyNyA3LjAzWiIgZmlsbD0iIzIwMkY1OCIvPgo8L3N2Zz4K);
+        background-repeat: no-repeat;
+        background-position-x: 90%;
+        background-position-y: 15px;
+      }
+    }
+  }
+  ul.form-submit{
+    display: flex;
+    margin: 0;
+    padding: 0;
+    li{
+      width: 50%;
+      label{
+        font-family: 'M PLUS Rounded 1c', sans-serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 12px;
+        line-height: 17px;
+        a{
+          color: #E63732;
+        }
+      }
+      input[type=checkbox]{
+        background: #FFFFFF;
+        border: 1px solid #FEA38E;
+        border-radius: 40px;
+        margin: 0 10px;
+      }
+      input[type=submit]{
+        background: #E63732;
+        border-radius: 40px;
+        border: 0;
+        padding: 10px 15px;
+        font-family: 'M PLUS Rounded 1c', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 18px;
+        width: 95%;
+        color: white;
+        float: right;
+      }    
+    }
+  }
+  
+`
